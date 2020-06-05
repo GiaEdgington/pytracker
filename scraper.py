a24b75ca9f62ab0e65e46de3727c6d427bddc3e4
giaedgington <gia.edgington@gmail.com>
2020-06-05 19:54:46 -0400

    import parser

--- scraper.py
+++ scraper.py
@@ -1,7 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://www.amazon.com/Sony-Full-Frame-Mirrorless-Interchangeable-Lens-ILCE7M3/dp/B07B43WPVK/ref=sr_1_2?dchild=1&keywords=sony+a7&qid=1591400636&sr=8-2'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+print(soup.prettify())
